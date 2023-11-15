10337194d01f011b7455309d94b8d3737167e918
jampierzcode <jampierv127@gmail.com>
2023-11-15 09:16:28 -0500

    ultimos cambios 1

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/proyectos/sumak-nature",
+  assetPrefix: "/proyectos/sumak-nature/",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
